f5898d1132d1edcad38529b39ad1bc3deb9f7fad
lcgaravito <lc.garavito@uniandes.edu.co>
2025-05-12 01:47:56 -0300

    loading-state

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,33 +1,41 @@
+import Image from "next/image";
+import styles from "./MainProducts.module.sass";
 const getProducts = async () => {
-  const response = await fetch(
-    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2025-04/products.json`,
-    {
-      method: "GET",
-      headers: {
-        "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
-        "Content-Type": "application/json",
-      },
-      cache: "no-store",
-    }
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `${process.env.SHOPIFY_HOSTNAME}/admin/api/2025-04/products.json`,
+      {
+        method: "GET",
+        headers: {
+          "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+      }
+    );
+    const { products } = await response.json();
+    return products;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return [];
+  }
 };
 export const MainProducts = async () => {
   const products = await getProducts();
-  console.log("products", products);
   return (
-    <section>
-      <h1>Main Products</h1>
-      {products.products.map((product: any) => (
-        <div key={product.id}>
-          <h2>{product.title}</h2>
-          <img width={200} src={product.images[0].src} alt={product.title} />
-
-          {product.body_html}
-          <p>Price: {product.variants[0].price}</p>
-        </div>
-      ))}
+    <section className={styles.MainProducts}>
+      <h3>✨ New products released!</h3>
+      <div className={styles.MainProducts__grid}>
+        {products.map((product: any) => {
+          const imageSrc = product.images[0].src;
+          return (
+            <article key={product.id}>
+              <p>{product.title}</p>
+              <Image src={imageSrc} fill alt={product.title} loading="eager" />
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
